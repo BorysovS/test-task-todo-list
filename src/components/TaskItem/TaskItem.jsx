6bce25e4fdc556c364c5c3f2ea8d6c50a087a8cf
Viacheslav Borysov <s.borisovbar@gmail.com>
2024-09-29 11:35:10 +0300

    d

--- src/components/TaskItem/TaskItem.jsx
+++ src/components/TaskItem/TaskItem.jsx
@@ -1,6 +1,6 @@
 import styles from './TaskItem.module.css';
 
-import sprite from './sprite.svg';
+import sprite from './assets/sprite.svg';
 
 export const TaskItem = ({ task, toggleCompleted, deleteTask }) => {
   return (
