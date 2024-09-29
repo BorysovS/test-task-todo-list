0c55037905e10414dc528a61d9f1a0e0ecec8f7b
Viacheslav Borysov <s.borisovbar@gmail.com>
2024-09-29 12:36:31 +0300

    add descr for empty tasks list

--- src/components/TaskList/TaskList.jsx
+++ src/components/TaskList/TaskList.jsx
@@ -5,6 +5,9 @@ import styles from './TaskList.module.css';
 export const TaskList = ({ tasks, toggleCompleted, deleteTask }) => {
   return (
     <ul className={styles.task_list}>
+      {tasks.length < 1 && (
+        <p>You don`t have any tasks or plans... it's impossible :)</p>
+      )}
       {tasks?.map(task => (
         <li key={task.id}>
           <TaskItem
